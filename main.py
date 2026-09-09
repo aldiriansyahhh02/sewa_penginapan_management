eeb857ee6b50d57c8199f99690ded7e712138fb9
aldiriansyahhh02 <aldiriansyah60@gmail.com>
2026-09-09 13:34:02 +0700

    Tambah read

--- main.py
+++ main.py
@@ -1,28 +1,63 @@
 # ===================================
-# [Your Program Title]
+# [Sewa Penginapan]
 # ===================================
-# Developed by. Bayu Prasetya
-# JCDS - [Class Batch]
+# Developed by. Muhammad Aldi Riansyah
+# JCDSBSD - [34]
 
 
 # /************************************/
 
 # /===== Data Model =====/
 # Create your data model here
-data = [] # Example data model
+data_kamar = [
+    {"id": 1, "tipe": "standar", "harga": 450000, "kasur": "Single"},
+    {"id": 2, "tipe": "standar", "harga": 450000, "kasur": "Single"},
+    {"id": 3, "tipe": "standar", "harga": 450000, "kasur": "Double"},
+    {"id": 4, "tipe": "deluxe", "harga": 550000, "kasur": "Double"},
+    {"id": 5, "tipe": "vip", "harga": 750000, "kasur": "Double"}
+] # Example data model
 
+id_terakhir = 5
 
 # /===== Feature Program =====/
 # Create your feature program here
-def read():
-    """Function for read the data
-    """
-    return
 
-def create():
-    """Function for create the data
-    """
-    return
+def tambah_kamar():
+    global id_terakhir
+    print("--- Tambah Kamar Baru ---")
+    tipe = input("Masukkan tipe kamar: ")
+    harga = input("Masukkan harga kamar: ")
+
+    while True:
+        try:
+            kasur = input("Masukkan Jenis Kasur: ")
+            if kasur != "Single" or kasur != "Double":
+                break
+            print("Kasur hanya terdiri dari Single atau Double")
+        except:
+            print("Masukkan angka yang valid")
+
+    id_terakhir = id_terakhir + 1 
+    kamar_baru = {
+        "id": id_terakhir,
+         "tipe": tipe,
+         "harga": harga,
+         "kasur": kasur
+    }
+    data_kamar.append(kamar_baru)
+    print(f"Kamar {tipe} telah didaftarkan dengan ID {id_terakhir}")
+
+def tampilkan_kamar():
+    print("\n--- DAFTAR SEMUA KAMAR ---")
+    if not data_kamar:
+        print("Belum ada kamar yang terdaftar")
+        return
+
+    print(f"{"ID":<5} | {"Tipe Kamar":<30} | {"Harga":<20} | {"Kasur":<10}")
+    print("-"*70)
+    for kamar in data_kamar:
+            print(f"{kamar["id"]:<5} | {kamar["tipe"]:<30} | {kamar["harga"]:<20} | {kamar["kasur"]:<10}")
+    print("-"*70)
 
 def update():
     """Function for update the data
@@ -36,22 +71,31 @@ def delete():
 
 # /===== Main Program =====/
 # Create your main program here
-def main():
-    """Function for main program
-    """
+while True:
+    print("\n=============================")
+    print("       SISTEM KELOLA KAMAR")
+    print("\n=============================")
+    print("1. Tambahkan Kamar")
+    print("2. Tampilkan Semua Kamar")
+    print("3. Cari Kamar")
+    print("4. Ubah Data Kamar")
+    print("5. Hapus Kamar")
+    print("6. keluar")
 
-    input_user = input("Insert your option: ")
-    if input_user == "1":
-        read()
-    elif input_user == "2":
-        create()
-    elif input_user == "3":
-        update()
-    elif input_user == "4":
-        delete()
+    pilih_menu = int(input("Pilih Menu: "))
+
+    if pilih_menu == 1:
+        tambah_kamar()
+    elif pilih_menu == 2:
+        tampilkan_kamar()
+    elif pilih_menu == 3:
+        pass
+    elif pilih_menu == 4:
+        pass
+    elif pilih_menu == 5:
+        pass
+    elif pilih_menu == 6:
+        print("Terima kasih telah menggunakan program ini, Sampai jumpa!")
+        break
     else:
-        print("Input is not valid !")
-
-
-if __name__ == "__main__":
-    main()
+        print("Input salah !")
