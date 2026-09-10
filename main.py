599bab8a80dcb4f5fb40fcb795e6142c83705f17
aldiriansyahhh02 <aldiriansyah60@gmail.com>
2026-09-10 15:56:41 +0700

    Add tampilkan_kamar

--- main.py
+++ main.py
@@ -7,20 +7,18 @@
 
 # /************************************/
 
-# /===== Data Model =====/
-# Create your data model here
+# /===== Data Kamar =====/
 data_kamar = [
-    {"id": 1, "tipe": "standar", "harga": 450000, "kasur": "Single"},
-    {"id": 2, "tipe": "standar", "harga": 450000, "kasur": "Single"},
-    {"id": 3, "tipe": "standar", "harga": 450000, "kasur": "Double"},
-    {"id": 4, "tipe": "deluxe", "harga": 550000, "kasur": "Double"},
-    {"id": 5, "tipe": "vip", "harga": 750000, "kasur": "Double"}
-] # Example data model
+    {"id": 1, "tipe": "standar", "harga": 450000, "kasur": "single", "status":"tersedia"},
+    {"id": 2, "tipe": "standar", "harga": 450000, "kasur": "single", "status":"tersedia"},
+    {"id": 3, "tipe": "standar", "harga": 450000, "kasur": "double", "status":"terisi"},
+    {"id": 4, "tipe": "deluxe", "harga": 550000, "kasur": "double", "status":"terisi"},
+    {"id": 5, "tipe": "vip", "harga": 750000, "kasur": "double", "status":"tersedia"}
+]
 
 id_terakhir = 5
 
-# /===== Feature Program =====/
-# Create your feature program here
+# /===== Program =====/
 
 def tambah_kamar():
     global id_terakhir
@@ -30,19 +28,31 @@ def tambah_kamar():
 
     while True:
         try:
-            kasur = input("Masukkan Jenis Kasur: ")
-            if kasur != "Single" or kasur != "Double":
-                break
-            print("Kasur hanya terdiri dari Single atau Double")
+            kasur = input("Masukkan Jenis Kasur (single/double): ")
+            if kasur != "single" and kasur != "double":
+                print("Kasur hanya terdiri dari ingle atau double")
+                continue
+            break
         except:
-            print("Masukkan angka yang valid")
+            print("Masukkan kasur yang valid")
+
+    while True:
+            try:
+                status = input("Masukkan Status kamar (tersedia/disewa): ")
+                if kasur != "tersedia" and kasur != "disewa":
+                    print("Status kamar hanya terdiri dari tersedia atau disewa")
+                    continue
+                break
+            except:
+                print("Masukkan kasur yang valid")
 
     id_terakhir = id_terakhir + 1 
     kamar_baru = {
         "id": id_terakhir,
          "tipe": tipe,
          "harga": harga,
-         "kasur": kasur
+         "kasur": kasur,
+         "status":status
     }
     data_kamar.append(kamar_baru)
     print(f"Kamar {tipe} telah didaftarkan dengan ID {id_terakhir}")
@@ -53,18 +63,17 @@ def tampilkan_kamar():
         print("Belum ada kamar yang terdaftar")
         return
 
-    print(f"{"ID":<5} | {"Tipe Kamar":<30} | {"Harga":<20} | {"Kasur":<10}")
+    print(f"{"ID":<5} | {"Tipe Kamar":<30} | {"Harga":<20} | {"Kasur":<10} | {"Status":<10}")
     print("-"*70)
     for kamar in data_kamar:
-            print(f"{kamar["id"]:<5} | {kamar["tipe"]:<30} | {kamar["harga"]:<20} | {kamar["kasur"]:<10}")
+            print(f"{kamar["id"]:<5} | {kamar["tipe"]:<30} | {kamar["harga"]:<20} | {kamar["kasur"]:<10} | {kamar["status"]:<10}")
     print("-"*70)
 
-def update():
-    """Function for update the data
-    """
-    return
+def ubah_kamar():
+    pass
 
-def delete():
+
+def hapus_kamar():
     """Function for delete the data
     """
     return
@@ -73,14 +82,13 @@ def delete():
 # Create your main program here
 while True:
     print("\n=============================")
-    print("       SISTEM KELOLA KAMAR")
+    print(" SISTEM KELOLA KAMAR PENGINAPAN")
     print("\n=============================")
     print("1. Tambahkan Kamar")
     print("2. Tampilkan Semua Kamar")
-    print("3. Cari Kamar")
-    print("4. Ubah Data Kamar")
-    print("5. Hapus Kamar")
-    print("6. keluar")
+    print("3. Ubah Data Kamar")
+    print("4. Hapus Kamar")
+    print("5. keluar")
 
     pilih_menu = int(input("Pilih Menu: "))
 
@@ -93,9 +101,7 @@ while True:
     elif pilih_menu == 4:
         pass
     elif pilih_menu == 5:
-        pass
-    elif pilih_menu == 6:
         print("Terima kasih telah menggunakan program ini, Sampai jumpa!")
         break
     else:
-        print("Input salah !")
+        print("Input salah ! Masukkan input yang benar")
